40bdab131b24c0d19c40c26a83d703710fb7dd4d
rrangel <v-rrangel@ean.com>
2017-04-25 18:28:17 -0500

    Adding basic configuration for js development environment

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpackage from 'webpack';
+import config from '../webpack.config.dev';
+
+const port = 3000;
+const app = express();
+const compiler = webpackage(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}));
+
+app.get("/", function(request, response){
+	response.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err){
+	if(err){
+		console.log(err);
+	}else {
+		open('http://localhost:'+ port);
+	}
+});
